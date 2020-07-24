e36a3f61f5cb8504cc23fbcf6d0f54ea53d14bd9
Daniel Faria <daniel.faria@phila.gov>
2020-07-24 15:58:42 -0400

    s3 deploy

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home3</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home Page</router-link> | <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body, html {
+  min-height: 100vh;
+  height: 100vh;
+}
 #app {
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: lightcoral;
+  min-height: 100vh;
+  height: 100vh;
 }
 </style>
